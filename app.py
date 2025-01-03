5df6a526035425e4e5b0251f3e1b59ca6d4726ae
unknown <lasjphd2@gmail.com>
2025-01-03 20:41:07 +0700

    feat: finish model v2

--- app.py
+++ app.py
@@ -1,48 +1,50 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import pandas as pd
+import numpy as np
 from typing import List, Any
-from book_recommender import BookRecommender
+from book_recommender_v2 import BookRecommender, WeightedRatingModel, GradientDescentExpModel, FavouriteDataset
 from fetch_data import get_books_data, get_user_info_data, get_favourite_books_data
 
 # --- Configuration and Model Loading ---
 app = FastAPI()
 
 # Precomputed data files
-CONTENT_EMBEDDINGS_PATH = "./preprocessed_data/content_embeddings.pkl"
-CONTENT_LABELS_PATH = "./preprocessed_data/content_labels.pkl"
-COTENT_WEIGHTS_PATH = "./preprocessed_data/content_w.pkl"  #
-CONTENT_PREPROCESSED_INPUT_PATH = (
-    "./preprocessed_data/content_preprocessed_input.pkl"  #
-)
-USER_EMBEDDINGS_PATH = "./preprocessed_data/user_embeddings.pkl"
-USER_LABELS_PATH = "./preprocessed_data/user_labels.pkl"
-USER_WEIGHTS_PATH = "./preprocessed_data/user_w.pkl"
-USER_PREPROCESSED_INPUT_PATH = "./preprocessed_data/user_preprocessed_input.pkl"
-FAVORITE_BOOKS_PREPROCESSED_INPUT_PATH = (
-    "./preprocessed_data/favorite_preprocessed_input.pkl"
-)
-SENTENCE_TRANSFORMER_PATH = "./preprocessed_data/sentence_transformer.pkl"
+RATING_N_PATH = "preprocessed_data/rating_N.pkl"
+RATING_DATASET_PATH = "preprocessed_data/rating_dataset.pkl"
+RATING_COUNT_THRESHOLD_PATH = "preprocessed_data/rating_count_threshold.pkl"
+RATING_AVG_MEAN_PATH = "preprocessed_data/rating_avg_mean.pkl"
 
-# Dataset files
-CONTENT_DATASET_PATH = "./dataset/content_based_data.csv"
-USER_DATASET_PATH = "./dataset/user_based_data.csv"
-FAVORITE_BOOKS_PATH = "./dataset/favo_books.csv"
+CONTENT_N_PATH = "preprocessed_data/content_N.pkl"
+CONTENT_INPUTS_PATH = "preprocessed_data/content_inputs.pkl"
+CONTENT_IDS_PATH = "preprocessed_data/content_ids.pkl"
+CONTENT_LABELS_PATH = "preprocessed_data/content_labels.pkl"
+CONTENT_W_PATH = "preprocessed_data/content_W.pkl"
+
+USER_N_PATH = "preprocessed_data/user_N.pkl"
+USER_INPUTS_PATH = "preprocessed_data/user_inputs.pkl"
+USER_IDS_PATH = "preprocessed_data/user_ids.pkl"
+USER_LABELS_PATH = "preprocessed_data/user_labels.pkl"
+USER_W_PATH = "preprocessed_data/user_W.pkl"
+
+FAVOURITE_DATASET_PATH = "preprocessed_data/favourite_dataset.pkl"
 
 try:
-    recommender = BookRecommender(sentence_transformer_path=SENTENCE_TRANSFORMER_PATH)
-    recommender.load_precomputed_data(
-        # Load content data
-        content_embeddings_path=CONTENT_EMBEDDINGS_PATH,
-        content_labels_path=CONTENT_LABELS_PATH,
-        content_weights_path=COTENT_WEIGHTS_PATH,
-        content_preprocessed_input_path=CONTENT_PREPROCESSED_INPUT_PATH,
-        user_embeddings_path=USER_EMBEDDINGS_PATH,
-        user_labels_path=USER_LABELS_PATH,
-        user_weights_path=USER_WEIGHTS_PATH,
-        user_preprocessed_input_path=USER_PREPROCESSED_INPUT_PATH,
-        favorite_preprocessed_input_path=FAVORITE_BOOKS_PREPROCESSED_INPUT_PATH,
-    )
+    rating_model = WeightedRatingModel()
+    rating_model.load_model(RATING_N_PATH, RATING_DATASET_PATH, RATING_COUNT_THRESHOLD_PATH, RATING_AVG_MEAN_PATH)
+
+    content_model = GradientDescentExpModel()
+    content_model.load_model(CONTENT_N_PATH, CONTENT_INPUTS_PATH, CONTENT_IDS_PATH, CONTENT_LABELS_PATH, CONTENT_W_PATH)
+
+    user_model = GradientDescentExpModel()
+    user_model.load_model(USER_N_PATH, USER_INPUTS_PATH, USER_IDS_PATH, USER_LABELS_PATH, USER_W_PATH)
+
+    favourite_dataset = FavouriteDataset()
+    favourite_dataset.load_dataset(FAVOURITE_DATASET_PATH)
+
+    # Initilize BookRecommender object
+    recommender = BookRecommender(rating_model, content_model, user_model, favourite_dataset)
+
 except Exception as e:
     print(f"Error loading precomputed data or Sentence Transformer: {e}")
     raise Exception("Error loading precomputed data or Sentence Transformer")
@@ -73,7 +75,7 @@ try:
             "img_url",
             "preview_url",
             "description",
-        ],
+        ]
     )
 
     user_df = pd.DataFrame(
@@ -88,72 +90,92 @@ try:
             "language",
             "factor",
             "goal",
-        ],
+        ]
     )
 
     favorite_books_df = pd.DataFrame(
-        favourite_books_data, columns=["user_id", "book_id", "added_at"]
+        favourite_books_data,
+        columns=[
+            "user_id",
+            "book_id",
+            "added_at"
+        ]
     )
-
-    # Update recommender with database data
-    recommender.books_data = content_df
-    recommender.user_info_data = user_df
-    recommender.favourite_books_data = favorite_books_df
-
 except Exception as e:
     print(f"Error loading data from database: {e}")
     raise Exception("Failed to load required data from database")
 
 
 # --- Pydantic Models ---
-class RecommendRequest(BaseModel):
-    book_id: int
-    n: int = 10
-
-
-class User_RecommendRequest(BaseModel):
-    user_id: str
-    n: int = 10
-
-
-class Recommendation(BaseModel):
-    id: str
-    title: str
-    authors: str
-    category: str
-    best_value: float
-
-
 class ResponseFormat(BaseModel):
     status: str
     message: str
     books: List[Any]
 
+class RecommendRequest(BaseModel):
+    book_id: int
+
+class User_RecommendRequest(BaseModel):
+    user_id: int
+
+class Recommendation(BaseModel):
+    id: int
+    title: str
+    authors: str
+    category: str
+    best_value: float
+
+def format_recommedation(books: list[int], best_values: np.ndarray=None) -> list[Recommendation]:
+    n_books = len(books)
+    if best_values is None:
+        best_values = np.array([-1 for _ in range(n_books)])
+
+    recommendations = []
+    for i in range(n_books):
+        # Check if book exists in dataframe
+        book_matches = content_df[content_df["id"].astype(int) == int(books[i])]
+        if book_matches.empty:
+            print(f"Warning: Book ID {books[i]} not found in database")
+            continue
+        try:
+            book_details = book_matches.iloc[0]
+            recommendations.append(
+                Recommendation(
+                    id=books[i],
+                    title=book_details["title"],
+                    authors=book_details["authors"],
+                    category=book_details["category"],
+                    best_value=best_values[i]
+                )
+            )
+        except IndexError as e:
+            print(f"Error accessing book details for ID {books[i]}: {str(e)}")
+            continue
+    if not recommendations:
+        raise ValueError("No valid recommendations found for the given book ID")
+    return recommendations
 
 # --- API Endpoints ---
 @app.get("/popular_books", response_model=ResponseFormat)
-def get_popular_books(n: int = 10):
+def get_popular_books():
     """
-    Endpoint to get the n most popular books.
+    Endpoint to get the most popular books.
     """
     try:
-        demographic_dataset = {
-            "id": content_df["id"].astype(str),
-            "avg_rating": content_df["avg_rating"].astype(float),
-            "rating_count": content_df["rating_count"].astype(int),
-        }
-        quantile_rate = 0.9
-        demographic_preprocessed_dataset = recommender.demographic_preprocess(
-            demographic_dataset, quantile_rate
-        )
-        demographic_preprocessed_df = pd.DataFrame(demographic_preprocessed_dataset)
-
-        popular_books = recommender.get_popular_books(demographic_preprocessed_df, n)
-
+        books, best_values = recommender.get_popular_books()
+        print("Recommended books:", books)
+        print("Best values:", best_values)
         return ResponseFormat(
             status="success",
             message="Popular books retrieved successfully",
-            books=popular_books,
+            books=format_recommedation(books, best_values)
+        )
+    except ValueError as e:
+        raise HTTPException(
+            status_code=404,
+            detail=ResponseFormat(
+                status="error", message=str(e), books=[]
+            ).dict(),  # Convert to dictionary for HTTPException
         )
     except Exception as e:
         raise HTTPException(
@@ -162,46 +184,32 @@ def get_popular_books(n: int = 10):
                 status="error", message=str(e), books=[]
             ).dict(),  # Convert to dictionary for HTTPException
         )
-
 
 @app.post("/recommend/content-based", response_model=ResponseFormat)
 def content_based_recommendations(req: RecommendRequest):
     """
     Endpoint to get book recommendations based on content-based filtering.
     """
+    # Check if book exists in dataframe
+    book_matches = content_df[content_df["id"].astype(int) == int(req.book_id)]
+    if book_matches.empty:
+        return ResponseFormat(
+            status="fail",
+            message=f"Warning: Book ID {req.book_id} not found in database",
+            books=[]
+        )
+    book_details = book_matches.iloc[0]
+    title=book_details["title"],
+    category=book_details["category"]
+
     try:
-        similar_books = recommender.find_similar_books(req.book_id, req.n)
-
-        recommendations = []
-        for book_id in similar_books:
-            # Check if book exists in dataframe
-            book_matches = content_df[content_df["id"].astype(str) == str(book_id)]
-            if book_matches.empty:
-                print(f"Warning: Book ID {book_id} not found in database")
-                continue
-                
-            try:
-                book_details = book_matches.iloc[0]
-                recommendations.append(
-                    Recommendation(
-                        id=book_id,
-                        title=book_details["title"],
-                        authors=book_details["authors"],
-                        category=book_details["category"],
-                        best_value=0.9
-                    )
-                )
-            except IndexError as e:
-                print(f"Error accessing book details for ID {book_id}: {str(e)}")
-                continue
-
-        if not recommendations:
-            raise ValueError("No valid recommendations found for the given book ID")
-
+        books, best_values = recommender.find_similar_books(int(req.book_id))
+        print("Recommended books:", books)
+        print("Best values:", best_values)
         return ResponseFormat(
             status="success",
-            message="Recommendations retrieved successfully",
-            books=recommendations,
+            message=f"Recommendations retrieved successfully for:\nBook ID: {req.book_id}\nTitle: {title}\nCategory: {category}",
+            books=format_recommedation(books, best_values)
         )
     except ValueError as e:
         raise HTTPException(
@@ -217,21 +225,31 @@ def content_based_recommendations(req: RecommendRequest):
                 status="error", message=str(e), books=[]
             ).dict(),  # Convert to dictionary for HTTPException
         )
-
 
 @app.post("/recommend/colabborative", response_model=ResponseFormat)
 def collaborative_recommendations(req: User_RecommendRequest):
     """
     Endpoint to get collaborative book recommendations based on user's reading preferences.
     """
-    try:
-        recommendations = recommender.get_collaborative_recommendations(
-            req.user_id, req.n
+    # Check if book exists in dataframe
+    user_matches = user_df[user_df["id"].astype(int) == int(req.user_id)]
+    if user_matches.empty:
+        return ResponseFormat(
+            status="fail",
+            message=f"Warning: User ID {req.user_id} not found in database",
+            books=[]
         )
+    user_details = user_matches.iloc[0]
+    goal=user_details["goal"]
+
+    try:
+        books, best_values = recommender.get_collaborative_recommendations(int(req.user_id))
+        print("Recommended books:", books)
+        print("Best values:", best_values)
         return ResponseFormat(
             status="success",
-            message="Collaborative recommendations retrieved successfully",
-            books=recommendations,
+            message=f"Collaborative recommendations retrieved successfully for:\nUser ID: {req.user_id}\nGoal: {goal}",
+            books=format_recommedation(books, best_values)
         )
     except ValueError as e:
         raise HTTPException(
@@ -240,7 +258,6 @@ def collaborative_recommendations(req: User_RecommendRequest):
                 status="error", message=str(e), books=[]
             ).dict(),  # Convert to dictionary for HTTPException
         )
-
     except Exception as e:
         raise HTTPException(
             status_code=500,
@@ -249,20 +266,37 @@ def collaborative_recommendations(req: User_RecommendRequest):
             ).dict(),  # Convert to dictionary for HTTPException
         )
 
-
 @app.post("/recommend/personalize", response_model=ResponseFormat)
 def personalized_recommendations(req: User_RecommendRequest):
     """
     Endpoint to get personalized book recommendations based on user's reading preferences.
     """
-    try:
-        recommendations = recommender.get_personalized_recommendations(
-            req.user_id, req.n
+    # Check if book exists in dataframe
+    user_matches = user_df[user_df["id"].astype(int) == int(req.user_id)]
+    if user_matches.empty:
+        return ResponseFormat(
+            status="fail",
+            message=f"Warning: User ID {req.user_id} not found in database",
+            books=[]
         )
+    user_details = user_matches.iloc[0]
+    goal=user_details["goal"]
+
+    try:
+        books, best_values = recommender.get_personalized_recommendations(int(req.user_id))
+        print("Recommended books:", books)
+        print("Best values:", best_values)
         return ResponseFormat(
             status="success",
-            message="Personalized recommendations retrieved successfully",
-            books=recommendations,
+            message=f"Personalized recommendations retrieved successfully for:\nUser ID: {req.user_id}\nGoal: {goal}",
+            books=format_recommedation(books, best_values)
+        )
+    except ValueError as e:
+        raise HTTPException(
+            status_code=404,
+            detail=ResponseFormat(
+                status="error", message=str(e), books=[]
+            ).dict(),  # Convert to dictionary for HTTPException
         )
     except Exception as e:
         raise HTTPException(
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,5 +1,7 @@
-from book_recommender import BookRecommender
+from book_recommender_v2 import RatingDataset, ContentDataset, UserDataset, FavouriteDataset, WeightedRatingModel, GradientDescentExpModel, Utils
 import pandas as pd
+import numpy as np
+
 from fetch_data import (
     get_books_data,
     get_user_info_data,
@@ -8,19 +10,26 @@ from fetch_data import (
 )
 
 # --- Configuration ---
-# Paths to your precomputed data files
 SENTENCE_TRANSFORMER_PATH = "preprocessed_data/sentence_transformer.pkl"
-CONTENT_EMBEDDINGS_PATH = "preprocessed_data/content_embeddings.pkl"
+
+RATING_N_PATH = "preprocessed_data/rating_N.pkl"
+RATING_DATASET_PATH = "preprocessed_data/rating_dataset.pkl"
+RATING_COUNT_THRESHOLD_PATH = "preprocessed_data/rating_count_threshold.pkl"
+RATING_AVG_MEAN_PATH = "preprocessed_data/rating_avg_mean.pkl"
+
+CONTENT_N_PATH = "preprocessed_data/content_N.pkl"
+CONTENT_INPUTS_PATH = "preprocessed_data/content_inputs.pkl"
+CONTENT_IDS_PATH = "preprocessed_data/content_ids.pkl"
 CONTENT_LABELS_PATH = "preprocessed_data/content_labels.pkl"
-CONTENT_WEIGHTS_PATH = "preprocessed_data/content_w.pkl"
-CONTENT_PREPROCESSED_INPUT_PATH = "preprocessed_data/content_preprocessed_input.pkl"
-USER_EMBEDDINGS_PATH = "preprocessed_data/user_embeddings.pkl"
+CONTENT_W_PATH = "preprocessed_data/content_W.pkl"
+
+USER_N_PATH = "preprocessed_data/user_N.pkl"
+USER_INPUTS_PATH = "preprocessed_data/user_inputs.pkl"
+USER_IDS_PATH = "preprocessed_data/user_ids.pkl"
 USER_LABELS_PATH = "preprocessed_data/user_labels.pkl"
-USER_WEIGHTS_PATH = "preprocessed_data/user_w.pkl"
-USER_PREPROCESSED_INPUT_PATH = "preprocessed_data/user_preprocessed_input.pkl"
-FAVORITE_BOOKS_PREPROCESSED_INPUT_PATH = (
-    "preprocessed_data/favorite_preprocessed_input.pkl"
-)
+USER_W_PATH = "preprocessed_data/user_W.pkl"
+
+FAVOURITE_DATASET_PATH = "preprocessed_data/favourite_dataset.pkl"
 
 # --- Load Data from Database ---
 def load_data_from_database():
@@ -49,17 +58,9 @@ def load_data_from_database():
                 "img_url",
                 "preview_url",
                 "description",
-            ],
+            ]
         )
-        # Handle published_date (extract year)
-        content_df["published_date"] = pd.to_datetime(
-            content_df["published_date"], errors="coerce"
-        ).dt.year.fillna(0).astype(int)  
 
-        # Ensure data type consistency
-        content_df["avg_rating"] = content_df["avg_rating"].astype(float)
-        content_df["rating_count"] = content_df["rating_count"].astype(int)
-        
         user_df = pd.DataFrame(
             user_info_data,
             columns=[
@@ -72,55 +73,81 @@ def load_data_from_database():
                 "language",
                 "factor",
                 "goal",
-            ],
+            ]
         )
         
-        # 
-        favorite_books_df = pd.DataFrame(
-            favourite_books_data, columns=["user_id", "book_id", "added_at"]
+        favourite_books_df = pd.DataFrame(
+            favourite_books_data,
+            columns=[
+                "user_id",
+                "book_id",
+                "added_at"
+            ]
         )
 
-        return content_df, user_df, favorite_books_df
+        return content_df, user_df, favourite_books_df
 
     except Exception as e:
         print(f"Error loading data from database: {e}")
         return None, None, None
 
-
-# --- Load and Preprocess Data ---
-try:
-    recommender = BookRecommender(sentence_transformer_path=SENTENCE_TRANSFORMER_PATH)
-except Exception as e:
-    print(f"Error loading Sentence Transformer: {e}")
-    print("Loading Sentence Transformer from scratch...")
-    recommender = BookRecommender()
-
 # Load data from the database
 print("Loading data from the database...")
-content_df, user_df, favorite_books_df = load_data_from_database()
+content_df, user_df, favourite_df = load_data_from_database()
 
-if content_df is None or user_df is None or favorite_books_df is None:
+if content_df is None or user_df is None or favourite_df is None:
     raise Exception("Failed to load data from the database.")
+
 
 # Preprocess and store embeddings
 print("Preprocessing and storing embeddings...")
-recommender.preprocess_and_store_book_embeddings(content_df)
-recommender.preprocess_and_store_user_embeddings(user_df)
-recommender.preprocess_and_store_favorite_books(favorite_books_df)
+encoding_model = 'all-mpnet-base-v2'
+scaler = 'min-max'
+
+rating_dataset = RatingDataset(content_df)
+print("Load Rating Dataset successfully")
+content_dataset = ContentDataset(content_df, SENTENCE_TRANSFORMER_PATH, scaler)
+print("Load Content Dataset successfully")
+user_dataset = UserDataset(user_df, SENTENCE_TRANSFORMER_PATH, scaler)
+print("Load User Dataset successfully")
+favourite_dataset = FavouriteDataset(favourite_df)
+print("Load Favourite Dataset successfully")
+
+
+# Fit dataset and train models
+rating_quantile_rate=0.9
+rating_N = 10
+rating_model = WeightedRatingModel(rating_quantile_rate, rating_N)
+rating_model.fit(rating_dataset)
+
+content_N=10
+content_initial_W=None
+content_lambda=np.array([1, 1, 2, 2, 1, 1, 1])
+content_learning_rate=0.045
+content_max_epoch=4
+content_model = GradientDescentExpModel(content_lambda, content_learning_rate, content_max_epoch, content_N)
+content_model.fit(content_dataset.get_inputs(), content_dataset.get_ids(), content_dataset.get_labels())
+
+content_W = content_model.train(content_initial_W)
+print("Content W:", content_W)
+
+user_N=10
+user_initial_W=None
+user_lambda=np.array([2, 2, 1, 1, 1, 1])
+user_learning_rate=0.06
+user_max_epoch=160
+user_model = GradientDescentExpModel(user_lambda, user_learning_rate, user_max_epoch, user_N)
+user_model.fit(user_dataset.get_inputs(), user_dataset.get_ids(), user_dataset.get_labels())
+
+user_W = user_model.train(user_initial_W)
+print("User W:", user_W)
+
 
 # Save Precomputed Data
 print("Saving precomputed data...")
-recommender.save_precomputed_data(
-    content_embeddings_path=CONTENT_EMBEDDINGS_PATH,
-    content_labels_path=CONTENT_LABELS_PATH,
-    content_weights_path=CONTENT_WEIGHTS_PATH,
-    content_preprocessed_input_path=CONTENT_PREPROCESSED_INPUT_PATH,
-    user_embeddings_path=USER_EMBEDDINGS_PATH,
-    user_labels_path=USER_LABELS_PATH,
-    user_weights_path=USER_WEIGHTS_PATH,
-    user_preprocessed_input_path=USER_PREPROCESSED_INPUT_PATH,
-    favorite_preprocessed_input_path=FAVORITE_BOOKS_PREPROCESSED_INPUT_PATH,
-)
-
+rating_model.save_model(RATING_N_PATH, RATING_DATASET_PATH, RATING_COUNT_THRESHOLD_PATH, RATING_AVG_MEAN_PATH)
+content_model.save_model(CONTENT_N_PATH, CONTENT_INPUTS_PATH, CONTENT_IDS_PATH, CONTENT_LABELS_PATH, CONTENT_W_PATH)
+user_model.save_model(USER_N_PATH, USER_INPUTS_PATH, USER_IDS_PATH, USER_LABELS_PATH, USER_W_PATH)
+favourite_dataset.save_dataset(FAVOURITE_DATASET_PATH)
 print("Preprocessing and data saving completed.")
 close_all_connections()
